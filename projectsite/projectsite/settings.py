37b82a636ec2c21c43fa782b78ee88c5bf953647
zel-06 <cymonjezrelb@gmail.com>
2026-03-13 02:44:55 +0800

    added bootstrap

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'tasktodomanager',
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +58,7 @@ ROOT_URLCONF = 'projectsite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR / 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,4 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
--- projectsite/tasktodomanager/views.py
+++ projectsite/tasktodomanager/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from django.views.generic.list import ListView
+from tasktodomanager.models import Category, Task 
+
 # Create your views here.
+class HomePageView(ListView):
+    model = Task
+    context_object_name = 'home'
+    template_name = "home.html"

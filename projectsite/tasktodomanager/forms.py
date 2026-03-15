4bc041afb7f2b602633245e50f5e44b648d35441
zel-06 <cymonjezrelb@gmail.com>
2026-03-16 02:22:56 +0800

    edit home UI

--- projectsite/tasktodomanager/forms.py
+++ projectsite/tasktodomanager/forms.py
@@ -5,4 +5,21 @@ from .models import Task
 class TaskForm(ModelForm):
     class Meta:
         model = Task
+        fields = ['title', 'description', 'deadline', 'category', 'priority']
+        widgets = {
+            'deadline': forms.DateTimeInput(attrs={
+                'type': 'datetime-local',
+                'class': 'form-control'
+            }),
+        }
+
+class TaskUpdateForm(ModelForm):
+    class Meta:
+        model = Task
         fields = "__all__"
+        widgets = {
+            'deadline': forms.DateTimeInput(attrs={
+                'type': 'datetime-local',
+                'class': 'form-control'
+            }),
+        }
